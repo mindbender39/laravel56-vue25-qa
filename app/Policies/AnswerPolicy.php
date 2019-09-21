b78d1fed95536cb76630778bd1e31dca4a9fb01c
Mindbender39 <mindbender39@gmail.com>
2019-09-22 01:12:30 +0500

    Implement update answer

---
+++ app/Policies/AnswerPolicy.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Answer;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class AnswerPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the answer.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Answer  $answer
+     * @return mixed
+     */
+    public function view(User $user, Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create answers.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the answer.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Answer  $answer
+     * @return mixed
+     */
+    public function update(User $user, Answer $answer)
+    {
+        return $user->id === $answer->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the answer.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Answer  $answer
+     * @return mixed
+     */
+    public function delete(User $user, Answer $answer)
+    {
+        return $user->id === $answer->user_id;
+    }
+
+    /**
+     * Determine whether the user can restore the answer.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Answer  $answer
+     * @return mixed
+     */
+    public function restore(User $user, Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the answer.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Answer  $answer
+     * @return mixed
+     */
+    public function forceDelete(User $user, Answer $answer)
+    {
+        //
+    }
+}
--- resources/views/answers/partials/_answer-list.blade.php
+++ resources/views/answers/partials/_answer-list.blade.php
@@ -6,9 +6,9 @@
                     <h2>{{$answersCount .' '. str_plural('Answer', $answersCount)}}</h2>
                 </div>
                 <hr>
-                
+
                 @include('partials._messages')
-                
+
                 @foreach($answers as $answer)
                     <div class="media">
                         <div class="d-flex flex-column vote-controls">
@@ -26,16 +26,41 @@
 
                         <div class="media-body">
                             {!! $answer->body_html !!}
-                            <div class="float-right">
+
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="ml-auto">
+                                        {{-- this authorization functionality created in AuthServiceProvider using Gate --}}
+                                        {{--@if (auth()->user()->can('update-question', $answer)) @endif--}}
+
+                                        {{-- this authorization functionality created in QuestionPolicy using Policy --}}
+                                        {{-- instead of using if statement and check with authenticated user we can just use @can directive --}}
+                                        @can ('update', $answer)
+                                            <a href="{{route('questions.answers.edit', [$question->id, $answer->id])}}" class="btn btn-outline-info btn-sm">Edit</a>
+                                        @endcan
+
+                                        {{--@if (auth()->user()->can('delete-question', $answer)) @endif--}}
+                                        @can ('delete', $answer)
+                                            <form class="d-inline-block" method="post" action="{{route('questions.answers.destroy', [$question->id, $answer->id])}}">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                            </form>
+                                        @endcan
+                                    </div>
+                                </div>
+                                <div class="col-4"></div>
+                                <div class="col-4">
                                         <span class="text-muted">
                                             Answered {{$answer->created_date}}
                                         </span>
-                                <div class="media mt-2">
-                                    <a href="{{$answer->user->url}}" class="pr-2">
-                                        <img src="{{$answer->user->avatar}}" alt="{{$answer->user->name}}">
-                                    </a>
-                                    <div class="media-body mt-1">
-                                        <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
+                                    <div class="media mt-2">
+                                        <a href="{{$answer->user->url}}" class="pr-2">
+                                            <img src="{{$answer->user->avatar}}" alt="{{$answer->user->name}}">
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
