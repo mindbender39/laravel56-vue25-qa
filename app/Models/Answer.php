9dbbceae8261f134f835befe467bde3f7a453eab
Mindbender39 <mindbender39@gmail.com>
2019-09-22 12:11:26 +0500

    Implement delete functionality for answer

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -40,5 +40,9 @@ class Answer extends Model
             // answers_count is a column of questions table
             $answer->question->increment('answers_count');
         });
+
+        static::deleted(function ($answer) {
+            $answer->question->decrement('answers_count');
+        });
     }
 }
